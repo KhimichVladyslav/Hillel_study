2ffb357bb13c60389d21ae64ac73b4fbb107568e
Vladyslav Khimich <vladkot1992@gmail.com>
2024-02-29 10:58:38 +0200

    fix:fixed and ref tasks

--- lesson_1/check_func_size.py
+++ lesson_1/check_func_size.py
@@ -1,16 +1,26 @@
 import sys
+import functools
+import tracemalloc
 from typing import Callable
 
 
-def check_function_size(func: Callable[[int, int], str]):
+def measure_memory(func: Callable) -> Callable:
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        func_result = func(*args, **kwargs)
-        size_of_the_func = sys.getsizeof(func)
-        return f'{func_result}\nThe size of the function is: {size_of_the_func} bytes'
+
+        tracemalloc.start()
+        result = func(*args, **kwargs)
+
+        memory_current, memory_peak = tracemalloc.get_traced_memory()
+
+        print(f"Memory used by '{func.__name__}':\ncurrent: {memory_current} bytes\npeak: {memory_peak} bytes")
+
+        return result
+
     return wrapper
 
 
-@check_function_size
+@measure_memory
 def sum_two_numbers(number_1: int, number_2: int) -> str:
     return f'The sum of {number_1} and {number_2} is {number_1+number_2}'
 
--- lesson_1/lfu_cache.py
+++ lesson_1/lfu_cache.py
@@ -6,33 +6,36 @@ from requests.exceptions import MissingSchema
 
 
 def cache(max_limit=2):
-    def internal(func):
-        @functools.wraps(func)
+    def internal(f):
+        @functools.wraps(f)
         def deco(*args, **kwargs):
             cache_key = (args, tuple(kwargs.items()))
 
             if cache_key in deco._cache:
-                deco._cache.move_to_end(cache_key, last=False)
+                deco._access_count[cache_key] += 1
+                deco._cache.move_to_end(cache_key, last=True)
                 return deco._cache[cache_key]
 
-            result = func(*args, **kwargs)
+            result = f(*args, **kwargs)
 
             if len(deco._cache) >= max_limit:
-                deco._cache.popitem(last=False)
-            deco._cache[cache_key] = result
-            print(deco._cache)
+                lfu_url = min(deco._access_count, key=deco._access_count.get)
+                deco._cache.pop(lfu_url)
+                deco._access_count.pop(lfu_url)
 
+            deco._cache[cache_key] = result
+            deco._access_count[cache_key] = 1
             return result
 
         deco._cache = OrderedDict()
+        deco._access_count = {}
 
         return deco
-
     return internal
 
 
 @cache()
-def fetch_url(url, first_n=100):
+def fetch_url(url: str, first_n: int = 100):
     """Fetch a given url"""
     try:
         res = requests.get(url)
